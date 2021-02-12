d3800b2398e6115340bd2af90e7a4f11762f4fb4
Nick Colonna <colonnan96@gmail.com>
2021-02-11 20:25:48 -0500

    Nearly functioning

--- breakout_game.py
+++ breakout_game.py
@@ -7,11 +7,13 @@
 import pygame
 import random
 
-display_w = 400		#width of game screen display
-display_h = 600		#height of game screen display
-ball_size = 10		#dimension of ball
+display_w = 400		
+display_h = 600		
+ball_size = 10
+ball_move = 5
 paddle_w = ball_size * 7
 paddle_h = 12
+paddle_move = 10
 brick_w = ball_size * 5
 brick_h = 15
 speed = 30
@@ -29,6 +31,7 @@ pygame.display.set_caption("BreakOut by Nick Colonna")
 pygame.display.update()
 display.fill(black)
 clock = pygame.time.Clock()
+font_style = pygame.font.SysFont("comicsansms", 20)
 
 class Block(pygame.sprite.Sprite):
 	def __init__(self, color, width, height):
@@ -68,7 +71,14 @@ def create_bricks():
 			count += 1
 
 """
-Main Game Loop
+Score keeping function to display score on screen
+""" 
+def get_score(score):
+	score_message = font_style.render("Score: {}".format(score), True, white)
+	display.blit(score_message, [5 , 1])
+
+"""
+Main game function containing game loop
 
 The core functionality of the game is run here
 """
@@ -77,8 +87,9 @@ def play_game():
 	paddle_x = display_w/2 - paddle_w/2
 	ball_x = display_w/2 - ball_size/2
 	ball_y = display_h - ball_size*5
-	ball_dx = 0
-	ball_dy = 0
+	ball_dx = random.choice([random.randint(-ball_move, -1), random.randint(1, ball_move)])
+	ball_dy = -ball_move
+	score = 0
 
 	#create the user paddle
 	paddle = Block(grey, paddle_w, paddle_h)
@@ -92,8 +103,10 @@ def play_game():
 	ball.rect.y = display_h - ball_size*5
 	all_sprites.add(ball)
 
+	#create bricks
 	create_bricks()
 
+	# Game Loop
 	while game_over == False:
 
 		for event in pygame.event.get():
@@ -101,16 +114,42 @@ def play_game():
 				game_over = True
 			elif event.type == pygame.KEYDOWN:	# look for left or right arrow key clicks
 				if event.key == pygame.K_LEFT:
-					paddle.rect.x += -10
+					if paddle.rect.x - paddle_move <= 0:
+						paddle.rect.x = 0
+					else:
+						paddle.rect.x += -paddle_move
 				elif event.key == pygame.K_RIGHT:
-					paddle.rect.x += 10
+					if paddle.rect.x + paddle_move >= (display_w - paddle_w):
+						paddle.rect.x = (display_w - paddle_w)
+					else:
+						paddle.rect.x += paddle_move
 
+		# check if ball hit any bricks hit
+		bricks_hit = pygame.sprite.spritecollide(ball, brick_list, True)
+		if bricks_hit != []:
+			ball_dy *= -1
+			score += len(bricks_hit)
 
-		ball_x += ball_dx
-		ball_y += ball_dy
+		# check if ball hit paddle
+		paddle_hit = pygame.sprite.collide_rect(ball, paddle)
+		if paddle_hit == True:
+			ball_dy *= -1
+
+		# check if ball hits display boundaries
+		if ball.rect.x + ball_dx <= 0:
+			ball_dx *= -1
+		elif ball.rect.x + ball_dx >= (display_w - ball_size):
+			ball_dx *= -1
+		if ball.rect.y + ball_dy <= 0:
+			ball_dy *= -1
+		
+		ball.rect.x += ball_dx
+		ball.rect.y += ball_dy
 
 		all_sprites.draw(display)
 
+
+		get_score(score)
 		pygame.display.update()
 		display.fill(black)
 		clock.tick(speed)
